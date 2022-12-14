345d089e4ccc8042e1a8c970b1ed49f68cf83863
maja4s91 <majastojanovic4@gmail.com>
2022-12-14 20:00:05 +0100

    Fix JS

--- script.js
+++ script.js
@@ -1,28 +1,16 @@
-// import "core-js/stable";
-
-// // polofilling async f
-// import "regenerator-runtime/runtime";
-
-// if (module.hot){
-//   module.hot accept();
-// }
-
 const btnDice = document.querySelector(".btn-dice");
-console.log(btnDice);
+const quoteId = document.querySelector(".quote-number");
+const quote = document.querySelector(".quote");
 
 const getQuote = async function () {
   try {
-    let res = await fetch("https://api.adviceslip.com/advice");
-    let slip = await res.json();
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const slip = await res.json();
 
     const slipId = slip.slip.id;
     const slipAdvice = slip.slip.advice;
-    console.log(slipId, slipAdvice);
 
-    const quoteId = document.querySelector(".quote-number");
     quoteId.innerHTML = `ADVICE #${slipId}`;
-
-    const quote = document.querySelector(".quote");
     quote.innerHTML = `"${slipAdvice}"`;
   } catch (err) {
     console.error(err);
@@ -35,7 +23,3 @@ btnDice.addEventListener("click", function (e) {
   e.preventDefault();
   getQuote();
 });
-
-// const renderQuote = function (slip) {
-//   const quote = slip.advice;
-// };
